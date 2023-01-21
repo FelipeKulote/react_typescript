4832ac27d606f765412a4debb28b0dc79b20dd13
Felipe Augusto <lipe_augustobq@hotmail.com>
2023-01-21 11:52:06 -0300

    feat: connection api with login form

--- src/components/pages/login.tsx
+++ src/components/pages/login.tsx
@@ -1,22 +1,43 @@
-import { FormEvent } from "react";
+import { FormEvent, useState } from "react";
 import { StyledLogin } from "./styled.login";
+import { FaEyeSlash, FaEye } from "react-icons/fa";
+import { api } from "../../utils/api/api";
 
 export function Login() {
-  function handleSubmit(e: FormEvent<HTMLFormElement>) {
+  const [showPassword, setShowPassword] = useState<boolean>(true);
+
+  function handleShowPassword() {
+    setShowPassword(!showPassword);
+  }
+
+  async function handleSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
     const loginPayload = {
       email: e.currentTarget.email.value,
       password: e.currentTarget.password.value,
     };
-    console.log(loginPayload);
+
+    const userData = await api.login(loginPayload);
+
+    console.log(userData)
   }
 
   return (
     <div>
       <StyledLogin onSubmit={handleSubmit}>
         <h2>LOGIN</h2>
-        <input type="text" placeholder="E-mail" name="email" />
-        <input type="password" placeholder="Senha" name="password" />
+        <input type="text" placeholder="E-mail" name="email" required />
+        <div>
+          <input
+            type={showPassword ? "password" : "text"}
+            placeholder="Senha"
+            name="password"
+            required
+          />
+          <button onClick={handleShowPassword}>
+            {showPassword ? <FaEyeSlash /> : <FaEye />}
+          </button>
+        </div>
         <button type="submit">ENVIAR</button>
       </StyledLogin>
     </div>
