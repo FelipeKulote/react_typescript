5ec3d843d990780437a780eed7d8edb2466e4351
Felipe Augusto <lipe_augustobq@hotmail.com>
2023-01-26 11:54:52 -0300

    feat: deploy with railway URL

--- src/utils/api/api.ts
+++ src/utils/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { LoginRequest } from "../types/requests";
 
-axios.defaults.baseURL = "http://localhost:3000";
+axios.defaults.baseURL = "https://projeto-nestjs-production.up.railway.app/api";
 axios.defaults.headers.post["content-Type"] = "application/json";
 
 axios.interceptors.request.use(
